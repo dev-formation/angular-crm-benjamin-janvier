57855040bf4de7b5bc5aa8f8d41aa51a98391906
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2022-01-25 14:34:31 +0100

    21 - template avec title

---
+++ src/app/templates/components/template-container/template-container.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-container',
+  templateUrl: './template-container.component.html',
+  styleUrls: ['./template-container.component.scss']
+})
+export class TemplateContainerComponent implements OnInit {
+  public title: string; 
+
+  constructor() {
+    this.title = "Oooh le jolie titre !";
+   }
+
+  ngOnInit(): void {
+  }
+
+}
