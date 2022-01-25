81ed7adb8b39f824c0a317a27b4122edac207ee0
Jérémy Bojko macMini <jeremy.bojko@protonmail.com>
2022-01-25 11:59:46 +0100

    18 - enums + interface + class

---
+++ src/app/core/models/order.ts
@@ -0,0 +1,16 @@
+import { StateOrder } from "../enums/state-order";
+import { OrderI } from "../interfaces/order-i";
+
+export class Order implements OrderI{
+    tjmHt = 1200;
+    nbJours = 5;
+    tva = 20;
+    state = StateOrder.OPTION;
+    typePresta!: string;
+    client!: string;
+    comment!: string;
+    id!: number;
+    constructor(obj?: Partial<Order>) {
+        if(obj){Object.assign(this, obj)}
+    }
+}
